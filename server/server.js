208728b8f64af1c3a36a31aabdc5b60abcc64122
5h44n <ahnaf430@gmail.com>
2025-08-22 04:43:36 +0600

    Update dotenv dependency to version 17.2.1 and adjust server port configuration

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
+import { config } from 'dotenv';
 import express from 'express';
 
+config();
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+app.use(express.json());  // Parse JSON bodies
 
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT} http://localhost:${PORT}`));
