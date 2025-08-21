0033137eda73663ebe8f556b727c431e6bffc54e
5h44n <ahnaf430@gmail.com>
2025-08-22 05:27:37 +0600

    Add bcrypt dependency for password hashing and configure authentication routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import { config } from 'dotenv';
 import express from 'express';
+import authRoutes from'./routes/authRoutes.js';
 
 config();
 const app = express();
 
 app.use(express.json());  // Parse JSON bodies
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} http://localhost:${PORT}`));
