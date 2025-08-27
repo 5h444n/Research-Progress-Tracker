5dbdaf9446970b282726947bb41ca63b8552c71f
5h44n <ahnaf430@gmail.com>
2025-08-27 14:31:35 +0600

    Add Document model and integrate upload routes in server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import winston from 'winston';
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient} from '@prisma/client';
 import authRoutes from './routes/authRoutes.js';
 import projectRoutes from "./routes/projectRoutes.js";
-import { logger } from './utils/logger.js';
+import {logger} from './utils/logger.js';
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 config();
 const app = express();
@@ -22,18 +23,19 @@ app.use(helmet());
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
-    message: { error: 'Too many requests, please try again later.' },
+    message: {error: 'Too many requests, please try again later.'},
     keyGenerator: (req) => req.user?.userId || req.ip, // Limit per user if authenticated
 });
 
 app.use(express.json());
 app.use('/api', limiter);
 app.use('/api/auth', authRoutes);
-app.use('/api',projectRoutes)
+app.use('/api', projectRoutes);
+app.use('/api', uploadRoutes);
 
 app.use((err, req, res, next) => {
     logger.error(err.stack);
-    res.status(500).json({ error: 'Something went wrong!' });
+    res.status(500).json({error: 'Something went wrong!'});
 });
 
 async function startServer() {
@@ -47,6 +49,7 @@ async function startServer() {
     }
 }
 
+console.log(process.env.UPLOAD_DIR);
 startServer();
 
 process.on('SIGTERM', async () => {
